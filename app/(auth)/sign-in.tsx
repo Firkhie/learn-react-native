6afeb3f4e03b3a8dc23a9aabc1360289add7720d
Firdig Alfalakhi <Alfalakhie@gmail.com>
2024-09-17 06:05:31 +0700

    first commit

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,71 @@
+import { View, Text, ScrollView, Image } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "@/constants";
+import FormField from "@/components/form-field";
+import CustomButton from "@/components/custom-button";
+import { Link } from "expo-router";
+
+const SignIn = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const submit = () => {};
+
+  return (
+    <SafeAreaView style={{ height: "100%", backgroundColor: "#161622" }}>
+      <ScrollView>
+        <View className="w-full justify-center min-h-[90vh] px-4 my-6">
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[115px] h-[35px]"
+          />
+          <Text className="text-2xl text-white font-psemibold mt-10">
+            Log in to Aora
+          </Text>
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e: string) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+            placeholder="john.doe@example.com"
+          />
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e: string) => setForm({ ...form, password: e })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+            placeholder="******"
+          />
+
+          <CustomButton
+            title="Sign In"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
+
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Don't have account ?{" "}
+              <Link
+                href="/sign-up"
+                className="text-lg font-psemibold text-secondary"
+              >
+                Sign Up
+              </Link>
+            </Text>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default SignIn;
---
+++ components/custom-button.tsx
@@ -0,0 +1,34 @@
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+
+type CustomButtonProps = {
+  title: string;
+  handlePress: any;
+  containerStyles?: string;
+  textStyles?: string;
+  isLoading?: boolean;
+};
+
+const CustomButton = ({
+  title,
+  handlePress,
+  containerStyles,
+  textStyles,
+  isLoading,
+}: CustomButtonProps) => {
+  return (
+    <TouchableOpacity
+      className={`bg-secondary justify-center items-center min-h-[62px] rounded-xl ${containerStyles} ${
+        isLoading ? "opacity-50" : ""
+      }`}
+      disabled={isLoading}
+      onPress={handlePress}
+      activeOpacity={0.7}
+    >
+      <Text className={`text-primary font-psemibold text-lg ${textStyles}`}>
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+export default CustomButton;
